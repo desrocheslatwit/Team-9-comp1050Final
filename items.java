21098a66e5c1f44b53019dbba494d504037ccb2a
Luna DesRoches <desrochest@wit.edu>
2024-11-27 11:15:34 -0500

    Implimented returns for item details

--- items.java
+++ items.java
@@ -1,9 +1,50 @@
 public class items {
     private String name = new String();
-    public items(String name) {
+    private String description = new String();
+    private int stock = 0;
+    private double price = 0.00;
+    private int quantity = 0;
+
+    public items(String name, String description, int stock, double price, int quantity) {
         this.name = name;
+        this.description = description;
+        this.stock = stock;
+        this.price = price;
+        this.quantity = quantity;
         
     }
+
+    // Methods to return different values to be used in the gui. 
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+    
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    // TODO: Create 
+    public String getFilePath() {
+        return "";
+    }
+
+    
+
+
+
+
 }
 
 /*
