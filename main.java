0e9a647a92b7913f1599d56ff6c6b4ea2bdfd9cc
Darjen Bejko <bejkod@wit.edu>
2024-12-03 18:32:18 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,5 +1,12 @@
-public class main {
-    public main(String[] args) {
-      
-  }
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Launch the GUI application in the Swing event-dispatching thread
+        SwingUtilities.invokeLater(() -> {
+            Gui gui = new Gui(); // Create the GUI instance
+            gui.setVisible(true); // Make the GUI visible
+        });
+    }
 }
